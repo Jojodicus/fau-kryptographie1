d11f1ea013f29df75d2192402fe99ca370a8a90e
jojodicus <jojodicus@gmail.com>
2024-10-21 19:04:43 +0200

    revert content in main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,4 @@
 #! /usr/bin/env python3
 
 if __name__ == '__main__':
-    import ciphers.substitution.caesar
-    c = ciphers.substitution.caesar.Caesar(4)
-    print(c.decode("SECRETMESSAGE"))
     print('TODO')
